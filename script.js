72f600dac6295d28d4838ccca4a029eb58b83f01
Hannali Patidar <hannalip@gmail.com>
2019-12-04 15:43:20 -0500

    got the password to randomize. the password shows up in the box.

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
-// function values {
-
-// }
-
-// User questions for password
-var promptLength = prompt("How many chartacters would you like your password to contain? (8 -124)");
-var tagLength = parseInt(promptLength,10)
-// if (lengthtag > "124") {
-// alert("The number is to large, pick a smaller number");
-// if (lengthtag < "8") {
-// alert("The number is to small, pick a bigger number");
-
-var tagSpecial = confirm("Click OK to confirm including special characters?");
-
-var tagNumeric = confirm("Click OK to confirm including numeric characters?");
-
-var tagLower = confirm("Click OK to confirm including lowercase characters?");
-
-var tagUpper = confirm("Click OK to confirm including uppercase characters?");
-
-// // function
-// function generate {}
-
-
 // possible password values
 var special = "!@#$%&*()_+-=[]|,./?><";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var digits = "0123456789";
 
+// User questions for password
+
+var promptLength = prompt("How many chartacters would you like your password to contain? (8 - 124)");
+
+var tagLength = parseInt(promptLength, 10);
+
+
+var tagSpecial = confirm("Click OK to confirm including special characters?");
+console.log(tagSpecial);
+
+if (tagSpecial === true) {
+    tagSpecial = special;
+}
+else {
+    special = "";
+}
+
+var tagNumeric = confirm("Click OK to confirm including numeric characters?");
+console.log(tagNumeric);
+
+if (tagNumeric === true) {
+    tagNumeric = digits;
+}
+else {
+    digits = "";
+}
+
+var tagLower = confirm("Click OK to confirm including lowercase characters?");
+console.log(tagLower);
+
+if (tagLower === true) {
+    tagLower = lower;
+}
+else {
+    lower = "";
+}
+
+var tagUpper = confirm("Click OK to confirm including uppercase characters?");
+console.log(tagUpper);
+
+if (tagUpper === true) {
+    tagUpper = upper;
+}
+else {
+    upper = "";
+}
+
+
 var generateBut = document.getElementById("generate");
 var copyBut = document.getElementById("copy")
 var pwdResult = document.getElementById("password")
 
-// function charTotal() {
-    var characters = special + lower + upper + digits;
+// chartacter equation
+var characters = (special + digits + lower + upper);
 
-    if tagSpecial === true
-    
-
-
+// getting the code to randomize
 
 function password(tagLength, characters) {
     var pwd = "";
@@ -49,31 +70,20 @@ function password(tagLength, characters) {
     }
 
     return pwd;
-
 }
 
-
-
-
-
-// var password = ""
-
-// if (lengthtag !== "")
-// alert("please enter a number between 8 - 124");
-// else {
-
-// }
-
-// Random rnd = new Random();
-
-// int n1 = rnd.nextInt(125 - 8) + 8;
-
-
+// //add password to display
+pwdResult.value = password(tagLength, characters);
 
 // TO GENERATE A NEW PASSWORD WHEN YOU CLICK
+function refreshPage() {
+    window.location.reload();
+}
+generateBut.addEventListener("click", refreshPage);
 
-// submitEl.addEventListener("click", function());
-
-
-// //add password to display
-// document.getElementById("password").value = password;
+// // To Copy to Clipboard
+// function copyMe () {
+//     copyText.select();
+//     document.execCommand("generate");
+//   }
+// copyBut.addEventListener("click",copyMe);
